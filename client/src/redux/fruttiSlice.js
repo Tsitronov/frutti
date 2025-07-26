b14f43acf8974c27175cf0f3fe2585ef43218754
Tsitronov <126343104+Tsitronov@users.noreply.github.com>
2025-07-26 16:16:46 +0200

    Update fruttiSlice.js

--- client/src/redux/fruttiSlice.js
+++ client/src/redux/fruttiSlice.js
@@ -2,7 +2,7 @@ ndimport { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 // URL del backend
-const URL = "https://frutti.onrender.com/api/frutti";
+const URL = "https://frutti-backend.onrender.com/api/frutti";
 //const URL = "http://localhost:3001/api/frutti";
 
 // 📥 Carica frutti dal server
