7ac0a041719e927b5bbc4a16aaa96d09cb850e7c
evgenii <tsitronov2017@gmail.com>
2025-11-06 20:25:04 +0100

    nov63c

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/pages/Login';
 import SulSito from './components/pages/SulSito';
 import Generale from './components/pages/Generale';
