26aa820c1e08ce8a77a8a433128594dec3775428
evgenii <tsitronov2017@gmail.com>
2025-11-10 23:53:15 +0100

    nov10c

--- client/src/components/UI/navbar/Navbar.jsx
+++ client/src/components/UI/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../../../context';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { toggleTheme } from "../../../redux/themeSlice";
-import { setTokens } from '../../../api.js';
+import api, { setTokens }from '../../../api.js';
 
 const Navbar = () => {
   const { isAuth, setIsAuth, categoria: contextCategoria, setCategoria } = useContext(AuthContext);
